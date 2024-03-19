aaf8e2c60ef7fc4d0dc87c2341cd682bfc69e4a4
mvelasquezmontoya <martin.velasquez.montoya@gmail.com>
2024-03-19 16:32:10 -0500

    Abrí script

--- scripts/Script.R
+++ scripts/Script.R
@@ -1,2 +1,27 @@
-#Nuevo documento
+# 
+# Big Data y Machine Learning para Economia Aplicada
+# Maria Camila Arias, Martin Velasquez, Mario Velasquez, Daniela Vlasak
+# Problem Set 2
+# 
+
+# Initial Setup -----------------------------------------------------------
+
+rm(list = ls())
+if(!require(pacman)) install.packages("pacman") ; require(pacman)
+
+
+p_load()
+
+
+
+
+# Initial Data Manipulation -----------------------------------------------
+
+
+
+
+
+
+# Logit -------------------------------------------------------------------
+
 
